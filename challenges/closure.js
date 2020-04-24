ecefd3f7622c4ad3e1a2ed675aad290cfd43df3f
lateralcreativity <lateralcreativity@outlook.com>
2020-04-24 10:45:04 -0500

    Finished closure section

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,21 @@ myFunction();
 
 // Explanation: 
 
+// nestedFunction is able to access internal because myFunction is the parent of it
+// functions cannot look down for variables but they can use variables declared above them
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function sumation(num){
+  let counter = 0;
+
+  while(num > 0){
+    counter += num--;
+  }
+
+  return counter;
+}
+console.log(sumation(4)); // 10
+console.log(sumation(10)); // 55
+console.log(sumation(25)); // 325
